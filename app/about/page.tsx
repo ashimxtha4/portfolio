df284cf478e8f70d49f1a59988fb48b881815e19
ashimxtha4 <ashim.xtha2000@gmail.com>
2025-03-11 20:31:37 +0545

    about page changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,51 @@
-"use client";
+'use client';
 
-import ScrollAnimation from "@/component/animatedCard";
-import "./about.scss";
-import { Icon } from "@iconify/react/dist/iconify.js";
-import Link from "next/link";
+import ScrollAnimation from '@/component/animatedCard';
+import './about.scss';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import Link from 'next/link';
 
 export default function about() {
+  const experiences: any[] = [
+    {
+      date: 'February 2022 - May 2022',
+      post: 'Software Developer Intern',
+      company: 'Asterdio',
+      description: 'Description',
+    },
+    {
+      date: 'May 2022 - June 2023',
+      post: 'Software Developer (Front-End)',
+      company: 'Asterdio',
+      description: 'Description',
+    },
+    {
+      date: 'July 2023 - December 2023',
+      post: 'Software Developer (Front-End)',
+      company: 'Freelance',
+      description: 'Description',
+    },
+    {
+      date: 'Febuary 2024 - Currently',
+      post: 'Software Developer (Full Stack)',
+      company: 'Kodiary Technologies',
+      description: 'Description',
+    },
+  ];
+  const education: any[] = [
+    {
+      date: '2019 - 2022',
+      post: 'Bachelor in Computer Science and Engineering',
+      company: 'Sree Vidyanikehan Enginnering College(JNTU Anantapur)',
+      description: 'Description',
+    },
+    {
+      date: '2016 - 2018',
+      post: ' +2 Science',
+      company: 'D.A.V',
+      description: 'Description',
+    },
+  ];
   return (
     <>
       <div className="preloader"></div>
@@ -22,11 +62,7 @@ export default function about() {
                     <Icon icon="et:linkedin" height={50} width={50} />
                   </Link>
                   <Link className="icons" href="">
-                    <Icon
-                      icon="teenyicons:git-outline"
-                      width="50"
-                      height="50"
-                    />
+                    <Icon icon="teenyicons:git-outline" width="50" height="50" />
                   </Link>
                   <Link className="icons" href="">
                     <Icon icon="et:linkedin" height={50} width={50} />
@@ -37,46 +73,69 @@ export default function about() {
             </ScrollAnimation>
           </div>
         </div>
-        <div className="rightDiv">
+        <div className="rightDiv text-[#fff]">
           <article className="aboutMe">
-            <h2 className="title">ABOUT ME</h2>
+            <h2 className="text-[18px] font-semibold">ABOUT ME</h2>
             <p className="desc">
-              hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am
-              ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i
-              am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim
-              hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am
+              hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi
+              i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i
+              am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am ashim hi i am
               ashim
             </p>
           </article>
           <article className="exp">
-            <h2 className="title">EXPERIENCE</h2>
+            <h2 className="text-[18px] font-semibold">EXPERIENCE</h2>
             <div className="content">
-              <ScrollAnimation class="ashim">
-                <div className="group">
-                  <span className="date">2007-2017</span>
-                  <span className="inst">Jilla</span>
-                  <p className="desc">description</p>
-                </div>
-              </ScrollAnimation>
-
-              <div className="group">
-                <span className="date">2007-2017</span>
-                <span className="inst">Jilla</span>
-                <p className="desc">description</p>
-              </div>
+              {experiences.map((exp: any, index: number) => (
+                // <div></div>
+                <ScrollAnimation
+                  key={index}
+                  class="relative w-[100%] bg-[#0f0f0f] opacity-[0] ease duration-1250 scale[0.6]"
+                >
+                  <div className="group">
+                    <span className="text-[#BCBCBC] text-[16px] opacity-60">{exp?.date}</span>
+                    <span className="text-[#5B78F6] text-[18px]">{exp.post}</span>
+                    <span className="text-[#BCBCBC] text-[14px] opacity-60 font-normal">
+                      {exp?.company}
+                    </span>
+                    {/* <p className="text-[16px]"> {exp?.description}</p> */}
+                  </div>
+                </ScrollAnimation>
+              ))}
             </div>
           </article>
           <article className="exp">
-            <h2 className="title">EDUCATION</h2>
+            <h2 className="text-[18px] font-semibold">Education</h2>
             <div className="content">
-              <div className="group">
-                <span className="date">2007-2017</span>
-                <span className="inst">Jilla</span>
-                <p className="desc">description</p>
-              </div>
+              {education.map((edu: any, index: number) => (
+                <ScrollAnimation
+                  key={index}
+                  class="relative w-[100%] bg-[#0f0f0f] opacity-[0] ease duration-1250 scale[0.6]"
+                >
+                  <div className="group">
+                    <span className="text-[#BCBCBC] text-[16px] opacity-60">{edu?.date}</span>
+                    <span className="text-[#5B78F6] text-[18px]">{edu.post}</span>
+                    <span className="text-[#BCBCBC] text-[14px] opacity-60 font-normal">
+                      {edu?.company}
+                    </span>
+                    {/* <p className="text-[16px]"> {edu?.description}</p> */}
+                  </div>
+                </ScrollAnimation>
+              ))}
             </div>
           </article>
-          <article className="skills"></article>
+          <article className="skills">
+            <h2 className="text-[18px] font-semibold mb-[19px]">Skills</h2>
+            <div className='flex flex-wrap'>
+              <ScrollAnimation class="relative w-[100%] bg-[#0f0f0f] opacity-[0] ease duration-1250 scale[0.6] w-[50%]">
+                <div className="flex flex-col gap-2">
+                  <span className="text-[#BCBCBC] text-[14px] opacity-60 font-normal">90%</span>
+                  <span className="text-[#FFFFFF] font-normal text-[18px]">Java Script</span>
+                  {/* <p className="text-[16px]"> {edu?.description}</p> */}
+                </div>
+              </ScrollAnimation>
+            </div>
+          </article>
         </div>
       </div>
     </>
